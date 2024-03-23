592c8414f50db8374e2789deadc1b08ae3b24353
Eli <78963198+niceEli@users.noreply.github.com>
2024-03-23 12:41:47 -0400

    W

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@
 int main()
 {
 	fmt::println("{}", "where if the fibonacci sequence do you want to be");
-	std::string amount;
+	int amount;
 	std::cin >> amount;
 	const auto start = std::chrono::high_resolution_clock::now();
-	fmt::println("{}", fibonacci::fibCal(std::stoi(amount)));
+	fmt::println("{}", fibonacci::fibCal(std::stoi(fmt::to_string(amount))));
 	const auto stop = std::chrono::high_resolution_clock::now();
 	const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 	std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
